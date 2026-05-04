2cfd921ec1a2c9d84b5def16e7a03f3196333b73
simoneb101 <sboyd1501@live.com>
2026-05-04 16:05:12 -0500

    Delete sales_weather_omaha_pipeline/publication.sql

--- sales_weather_omaha_pipeline/publication.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM pg_create_logical_replication_slot('airbyte_gis_weather', 'pgoutput');
-DROP PUBLICATION IF EXISTS airbyte_gis_weather;
-CREATE PUBLICATION airbyte_gis_weather FOR TABLE data.gis_weather;
